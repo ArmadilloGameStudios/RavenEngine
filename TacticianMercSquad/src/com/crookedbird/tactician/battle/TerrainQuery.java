09b70b6fbff4c4be4af227e827787302dcd2e987
CookedBird <parkerpro@sbcglobal.net>
2016-09-12 23:56:23 -0500

    Added Missing Files

---
+++ TacticianMercSquad/src/com/crookedbird/tactician/battle/TerrainQuery.java
@@ -0,0 +1,44 @@
+package com.crookedbird.tactician.battle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.crookedbird.engine.database.GameData;
+import com.crookedbird.engine.database.GameDataQuery;
+
+public class TerrainQuery extends GameDataQuery {
+	private String theme;
+	private List<String> type;
+
+	public TerrainQuery(String theme, List<String> type) {
+		this.theme = theme;
+		this.type = new ArrayList<String>();
+
+		for (String t : type) {
+			this.type.add(t.toLowerCase());
+		}
+	}
+
+	@Override
+	public boolean matches(GameData row) {
+		GameData data = row.getData("type");
+
+		if (data.isList()) {
+			if (type.size() == data.getList().size()) {
+				boolean suc = true;
+
+				for (int i = 0; i < type.size() && suc; i++) {
+					suc &= type.contains(data.getList().get(i).getString()
+							.toLowerCase());
+				}
+
+				return suc;
+			}
+		} else if (data.isString()) {
+			if (type.size() == 1)
+				return data.getString().equalsIgnoreCase(type.get(0));
+		}
+
+		return false;
+	}
+}
